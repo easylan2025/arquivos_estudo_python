e83e3e97a82585886ea2a20040364c3d6a5987c3
easylan2025 <easylan@gmail.com>
2025-09-29 17:11:31 -0300

    codigos em python

---
+++ string_formatada.py
@@ -0,0 +1,20 @@
+itens = ['tv', 'som', 'outro_som']
+preco = [500, 600, 1500]
+
+
+custo_total = preco[0] + preco[1] + preco[2]
+print (itens[2], preco[2], custo_total)
+
+dolly = 500
+coca = 800
+peixe = 5080
+arroz = 18
+bombril = 900
+assolan = 49
+escova= 800
+
+cat_limpeza= ['bombril','assolan','escova',]
+
+cat_limpeza_qtd= [50, 6000, 800]
+
+print(f'A quantidade certa disso foi: {cat_limpeza[0]},{cat_limpeza_qtd[0]}')
---
+++ validador_email.py
@@ -0,0 +1,27 @@
+email = input('Digite seu E-mail: ').casefold().strip().replace(' ', '')
+email = email.replace(',', '.')
+
+# Verificação de email simples
+verificador_email = '@' in email and '.' in email.split('@')[-1]
+
+if verificador_email:
+    print('E-mail válido:', email)
+else:
+    print('E-mail inválido:', email)
+
+cpf = input('Digite o CPF: ').strip().replace('.', '').replace('-', '').replace(' ', '')
+
+# Verificação do CPF
+cpf_valido = len(cpf) == 11 and cpf.isnumeric()
+
+if cpf_valido:
+    print(cpf, 'Válido')
+else:
+    print(cpf, 'CPF Inválido')
+
+# Checagem final
+if verificador_email and cpf_valido:
+    print('✅ Dados corretos:', email, cpf ,'💖')
+else:
+    print('❌ Algo foi preenchido incorretamente')
+
