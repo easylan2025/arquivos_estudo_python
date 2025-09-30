d505c5b95beafb1b7bd079b81cd0ec66b1fbbaeb
easylan2025 <easylan@gmail.com>
2025-09-30 09:36:56 -0300

    Programa verificação estoque

---
+++ verifica_estoque.py
@@ -0,0 +1,10 @@
+prudutos = ['armario', 'tv', 'geladeira','microondas','fogao', 'ceular', 'fone', 'furadeira', 'notebook']
+estoque = [10, 50, 20, 35, 55, 19, 58, 47, 22]
+
+consulta = str(input('Digite o nome do produto para consultar o estoque: ')).strip().casefold()
+i = prudutos.index(consulta)
+
+if consulta == prudutos[i]:
+    print(f'A quantidade do produto: {consulta} é de {estoque[i]}')
+else:
+    print('O produto não existe na lista')    
