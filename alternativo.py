7106be3fa85e8d5fa2b170d9553adcf61aad27a7
MARCELO B. TEIXEIRA <easylan@gmail.com>
2025-10-02 08:45:45 -0300

    Update alternativo.py

--- alternativo.py
+++ alternativo.py
@@ -4,12 +4,14 @@ estoque = [
     [225,322,7457,11,685,3249,854,33,7861],
     [325,422,8457,211,685,3249,854,33,7862],
     [425,5227,97,311,685,3249,854,33,7863],
-    [525,622,10457,41,685,3249,854,33,11],
+    [525,622,10457,41,685,3249,854,33,79999],
 ]
 
 for i, estoque_fabrica in enumerate(estoque):
     fabrica = fabricas[i]
        
-    for j, qtd in enumerate(estoque_fabrica, start= 1):
+    for x, qtd in enumerate(estoque_fabrica, start= 1):
         if qtd < nivel_minimo:
-            print('A Fabrica: {}, possui o item: {} com estoque atual: {}'.format(fabrica, j, qtd))
+            print('A Fabrica: {}, possui o item: {} com estoque atual: {}'.format(fabrica, x, qtd))
+
+#o animal nao mecha ai 
