71bc03fb1e908dbcd4ef9f86f7c64c4c7a945cbb
Marcelo <esylan@gmail.com>
2025-10-03 10:11:44 -0300

    somando projetos ao for

--- for.py
+++ for.py
@@ -27,10 +27,21 @@ for i, venda in enumerate(vendas) :
         print(vendas[i])
 '''
 
+fabricas = ['Marcelo industries', 'china in box', 'marvel', 'ifood']
+nivel_minimo = 50
+estoque = [
+    [225,322,7457,11,685,3249,854,33,7861],
+    [325,422,8457,211,685,3249,854,33,7862],
+    [425,5227,97,311,685,3249,854,33,7863],
+    [525,622,10457,41,685,3249,854,33,20]
+]
 
-#mais codigo 
-            #mais codigo 
-
+for i, estoque_fabrica in enumerate(estoque):
+    fabrica = fabricas[i]
+       
+    for j, qtd in enumerate(estoque_fabrica, start= 1):
+        if qtd < nivel_minimo:
+            print('A Fabrica: {}, possui o item: {} com estoque atual: {}'.format(fabrica, j, qtd))
 
 
 
