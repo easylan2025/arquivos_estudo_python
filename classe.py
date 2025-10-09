16d2e73a9bca9a5e5413dce586234434fd79b9f0
Marcelo <esylan@gmail.com>
2025-10-09 11:01:04 -0300

    classe

---
+++ classe.py
@@ -0,0 +1,40 @@
+class Bicicleta:
+    def __init__(self, nome, modelo, cor):
+        self.nome=nome
+        self.cor=cor
+        self.modelo=modelo
+    
+def buzina(self):
+    print('show')
+    
+def parar(self):
+    print('upa upa')
+
+b1= Bicicleta('caloi', 'bmx' 'vermelho') 
+
+print(Bicicleta[b1])
+
+'''
+class Pessoa:
+    def __init__(self, nome, sobrenome, peso):
+        self.nome= nome
+        self.sobrenome= sobrenome
+        self.peso=peso
+
+    def tossir(self):
+        print('cof cof')
+
+    def escola(self):
+        print('já acordei')
+
+    def correr(self):
+        print('corra forest, corra')
+
+m1= Pessoa('Marcelo', 'Braga', 110)
+
+print(m1.nome, m1.peso, m1.sobrenome)
+
+m1.tossir()
+m1.escola()
+m1.correr()
+'''
